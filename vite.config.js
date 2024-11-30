32930bc5412aeea0b30051dc9839d1204205138f
BethTawiah <elizabeth.tawiah@amalitech.org>
2024-11-30 15:32:42 +0000

    fix bugs

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist', 
     rollupOptions: {
-      external: ['react-icons/fi']
-    }// Ensure the output directory is set correctly
+      external: [], 
+    },
   },
-  base: './', // Set base path for relative imports
-})
+  base: './', 
+});
