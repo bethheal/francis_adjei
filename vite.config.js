f334c076254fa10a410ecd3272391c4b839bc045
BethTawiah <elizabeth.tawiah@amalitech.org>
2024-11-30 13:15:25 +0000

    fix bugs

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Specify the output directory (default is 'dist')
-    rollupOptions: {
-      output: {
-        entryFileNames: `[name].js`, // Ensure files have proper .js extensions
-      },
-    },
+    outDir: 'dist', // Ensure the output directory is set correctly
   },
+  base: './', // Set base path for relative imports
 })
